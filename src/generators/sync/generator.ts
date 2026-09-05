d78d4a149397f9e0aa81d250a2de8e4b37b0241b
Gregory Bouteiller <gregory.bouteiller@gmail.com>
2026-09-05 10:19:10 +0400

    KEE-14 make guidance an Nx sync generator

--- src/generators/sync/generator.ts
+++ src/generators/sync/generator.ts
@@ -4,4 +4,5 @@ import { syncGuidance } from "../../guidance.ts";
 
 export default function syncGenerator(tree: Tree) {
   syncGuidance(tree);
+  return { outOfSyncMessage: "Keenko generated guidance is out of sync. Run 'nx sync'." };
 }
